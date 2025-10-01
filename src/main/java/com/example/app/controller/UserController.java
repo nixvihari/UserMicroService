53aa24bc0243710bd7bcd5a39f2099b7ffd434f3
Nikunj Vihari Konakalla <nikunjkonakalla@Pluto.local>
2025-10-01 15:31:31 +0530

    ADDED: update operation on user

--- src/main/java/com/example/app/controller/UserController.java
+++ src/main/java/com/example/app/controller/UserController.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -38,7 +39,12 @@ public class UserController {
 	}
 	
 	@DeleteMapping("/deleteUser/{userId}")
-	public void deleteUser(@PathVariable Long userId) {
+	public void deleteUser(@PathVariable("userId") Long userId) {
 		userService.deleteUser(userId);
+	}
+	
+	@PutMapping("/updateUser/{userId}")
+	public User updateUser(@PathVariable("userId") Long userId, @RequestBody User newUserDetails) {
+		return userService.updateUser(userId, newUserDetails);
 	}
 }
--- src/main/java/com/example/app/service/UserService.java
+++ src/main/java/com/example/app/service/UserService.java
@@ -5,7 +5,7 @@ import java.util.Optional;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
-
+import com.example.app.controller.UserController;
 import com.example.app.repo.User;
 import com.example.app.repo.UserRepository;
 
@@ -14,7 +14,17 @@ public class UserService {
 
 	@Autowired
 	UserRepository userRepository;
+
+
+	//Helpers
+	private void updateHelper(User newUserDetails, User existingUser) {
+		existingUser.setFirstName(newUserDetails.getFirstName());
+		existingUser.setLastName(newUserDetails.getLastName());
+		existingUser.setPhone(newUserDetails.getPhone());
+		existingUser.setEmail(newUserDetails.getEmail());
+	}
 	
+	//Basic CRUD
 	public List<User> getUsers() {
 		return userRepository.findAll();
 	}
@@ -29,5 +39,16 @@ public class UserService {
 	
 	public void deleteUser(Long userId) {
 		userRepository.deleteById(userId);
+	}
+	
+	public User updateUser(Long userId, User newUserDetails) {
+		Optional<User> existingUserOptional = getUserById(userId);
+		
+		if (existingUserOptional.isEmpty()) return null;
+		
+		User existingUser = existingUserOptional.get();
+		updateHelper(newUserDetails, existingUser);
+		
+		return addUser(existingUser);
 	}
 }
